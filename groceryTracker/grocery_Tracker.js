6c1a5ca53e34b09257c03992f3782a2adde240dc
Jokotolu <ajokotoluwa@gmail.com>
2024-05-01 08:37:07 -0400

    message

--- groceryTracker/grocery_Tracker.js
+++ groceryTracker/grocery_Tracker.js
@@ -1,12 +1,6 @@
-let length;
-let width;
-function calculateArea() {
-    length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('width').value);
-    let area = length * width;
-    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
-
-   }
+let grocery1;
+let grocery2;
+let grocery3;
    function groceryTracker() {
     grocery1 = parseFloat(document.getElementById('grocery1').value);
     grocery2= parseFloat(document.getElementById('grocery2').value);
