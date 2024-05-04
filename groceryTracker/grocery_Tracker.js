265614e9fc0ca630b9f48de2abf6e37c89d9c16c
Jokotolu <ajokotoluwa@gmail.com>
2024-05-04 10:05:49 -0400

    message

--- groceryTracker/grocery_Tracker.js
+++ groceryTracker/grocery_Tracker.js
@@ -5,7 +5,7 @@ let grocery3;
     grocery1 = parseFloat(document.getElementById('grocery1').value);
     grocery2= parseFloat(document.getElementById('grocery2').value);
     grocery3 = parseFloat(document.getElementById('grocery3').value);
-    let expenditure = grocery1 + grocery2 + grocery3;
-    document.getElementById('result').innerText = `The total amount is: ${expenditure}`;
+    const sum = grocery1 + grocery2 + grocery3;
+    document.getElementById('result').innerText = `The total amount is: ${sum}`;
     
    }
